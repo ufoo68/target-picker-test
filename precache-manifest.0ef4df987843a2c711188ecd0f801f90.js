bf3065c7984cdd035f6280baaa77b34f4388b8d1
ufoo68 <mitarou.yuutarou@gmail.com>
2020-03-29 23:47:13 +0900

    Updates

---
+++ precache-manifest.0ef4df987843a2c711188ecd0f801f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c403907b1001534912120311fc4bdc6",
+    "url": "/target-picker-test/index.html"
+  },
+  {
+    "revision": "0996138df87c55ce584c",
+    "url": "/target-picker-test/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "06f2d6f3509d22c25a69",
+    "url": "/target-picker-test/static/js/2.1ee6f23e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/target-picker-test/static/js/2.1ee6f23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0996138df87c55ce584c",
+    "url": "/target-picker-test/static/js/main.8a6ff682.chunk.js"
+  },
+  {
+    "revision": "53875cdef1eda929c488",
+    "url": "/target-picker-test/static/js/runtime-main.7c9c88c7.js"
+  }
+]);
